dcefc31f012c396a131bccc93fcc7688bf51c312
Tim Evci <tevci93@gmail.com>
2025-02-21 17:50:04 +0300

    feat: add home page components for hero, recent projects, work experience, and connect section

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@/types";
 
 export const SITE: Site = {
-  NAME: "tevci.dev",
+  NAME: "Timurcan Evci",
   EMAIL: "info@tevci.dev",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
@@ -10,7 +10,8 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION:
+    "Welcome to my personal website! I am a freelance software engineer based in Antalya, Turkey.",
 };
 
 export const WORK: Metadata = {
